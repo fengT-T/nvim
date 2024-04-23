7b32bbda98ea851bc23c6b24503581cbe606d0fc
feng <fengtxt@vip.qq.com>
2024-04-23 19:41:42 +0800

    add format

--- lua/util.lua
+++ lua/util.lua
@@ -99,6 +99,8 @@ return {
       javascript = { 'prettier' },
       vue = { 'prettier' },
       typescript = { 'prettier' },
+      html = { 'prettier' },
+      css = { 'prettier' },
     },
     format_on_save = { timeout_ms = 500, lsp_fallback = true },
     formatters = {},
