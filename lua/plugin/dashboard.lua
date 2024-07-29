093781b9eba812385cd82bbae242a5d68f6423ea
feng <fengtxt@vip.qq.com>
2024-07-29 16:12:02 +0800

    update dashboard

--- lua/plugin/dashboard.lua
+++ lua/plugin/dashboard.lua
@@ -26,15 +26,9 @@ return function()
         { action = 'lua require("telescope.builtin").old_files()', desc = " Recent Files", icon = " ", key = "r" },
         { action = 'lua require("telescope.builtin").live_grep()', desc = " Find Text", icon = " ", key = "g" },
         { action = 'lua require("persistence").load()', desc = " Restore Session", icon = " ", key = "s" },
-        { action = "LazyExtras", desc = " Lazy Extras", icon = " ", key = "x" },
         { action = "Lazy", desc = " Lazy", icon = "󰒲 ", key = "l" },
         { action = function() vim.api.nvim_input("<cmd>qa<cr>") end, desc = " Quit", icon = " ", key = "q" },
       },
-      footer = function()
-        local stats = require('lazy').stats()
-        local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-        return { '⚡ Neovim loaded ' .. stats.loaded .. '/' .. stats.count .. ' plugins in ' .. ms .. 'ms' }
-      end,
     },
   }
 
@@ -42,19 +36,5 @@ return function()
     button.desc = button.desc .. string.rep(' ', 43 - #button.desc)
     button.key_format = '  %s'
   end
-
-  -- open dashboard after closing lazy
-  -- if vim.o.filetype == 'lazy' then
-  --   vim.api.nvim_create_autocmd('WinClosed', {
-  --     pattern = tostring(vim.api.nvim_get_current_win()),
-  --     once = true,
-  --     callback = function()
-  --       vim.schedule(function()
-  --         vim.api.nvim_exec_autocmds('UIEnter', { group = 'dashboard' })
-  --       end)
-  --     end,
-  --   })
-  -- end
-
   return opts
 end
