a614d8b1ed6db683cd107a64ede8b29e681a0285
feng <fengtxt@vip.qq.com>
2024-09-06 16:38:33 +0800

    feat(neovide): use subpixelantialast p

--- lua/options.lua
+++ lua/options.lua
@@ -71,9 +71,9 @@ vim.opt.foldenable = true
 vim.opt.fileformats = 'unix,dos'
 vim.opt.termguicolors = true
 
-vim.opt.guifont = 'CaskaydiaCove Nerd Font:h13'
 --- neovide config
 if vim.g.neovide then
+  vim.opt.guifont = 'CaskaydiaCove Nerd Font:h13:e-subpixelantialias'
   vim.g.neovide_remember_window_size = true
   vim.g.neovide_cursor_antialiasing = true
   vim.g.neovide_scroll_animation_length = 0.1
