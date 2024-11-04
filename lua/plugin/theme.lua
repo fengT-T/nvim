190bb1b472fb41e99846b1c895f22221818c14d1
feng <fengtxt@vip.qq.com>
2024-11-04 18:35:55 +0800

    add yazi and terminal theme

--- lua/plugin/theme.lua
+++ lua/plugin/theme.lua
@@ -7,7 +7,7 @@ return {
   },
   github = {
     'projekt0n/github-nvim-theme',
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    lazy = false,    -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
   },
   catppuccin = {
@@ -22,6 +22,7 @@ return {
           base = '#ffffff',
         },
       },
+      term_colors = true,
     },
   },
   bluloco = {
