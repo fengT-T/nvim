62f86544b81ae2f4ef843e3d1f4025eb4a8b16f5
fengtxt <fengtxt@vip.qq.com>
2025-02-12 00:30:41 +0800

    cmp

--- lua/plugin/cmp.lua
+++ lua/plugin/cmp.lua
@@ -16,7 +16,7 @@ return {
       -- }
     },
     appearance = {
-      -- use_nvim_cmp_as_default = true,
+      use_nvim_cmp_as_default = true,
       nerd_font_variant = 'normal'
     },
     sources = {
