5d03b37fa0092a479a3de8591aed3a6be4ee042b
feng <fengtxt@vip.qq.com>
2025-07-25 11:09:10 +0800

    feat(fzf-lua): title

--- lua/plugin/fzf-lua.lua
+++ lua/plugin/fzf-lua.lua
@@ -7,7 +7,16 @@ return {
       fzf_colors = true,
       lsp = {
         code_actions = {
-          previewer = "codeaction_native"
+          previewer = "codeaction_native",
+          winopts = {
+            -- 关键：设置垂直布局（上下分割）
+            preview = {
+              layout = 'vertical', -- 垂直布局（上下分割）
+              vertical = 'up:70%', -- 预览窗口在上方，占65%高度（可调整比例）
+              -- vertical = 'down:40%',     -- 或放在下方
+              scrollbar = 'false' -- 可选的滚动条设置
+            }
+          }
         },
       },
       files = {
@@ -18,7 +27,6 @@ return {
           horizontal = "right:50%"
         }
       }
-
     },
     init = function()
       require("fzf-lua").register_ui_select()
