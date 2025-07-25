4121cfd22bca7440a30eab1652269d232682a303
feng <fengtxt@vip.qq.com>
2025-07-25 11:47:58 +0800

    feat(ai): title

--- lua/plugin/ai.lua
+++ lua/plugin/ai.lua
@@ -72,7 +72,7 @@ local ai_list = {
           api_key = function()
             return get_key('/home/feng/.aider.conf.yml', "openai%-api%-key:%s*['\"]?([%w%-_]+)['\"]?")
           end,
-          model= "deepseek-ai/DeepSeek-V3",
+          model = "deepseek-ai/DeepSeek-V3",
           name = ' ',
           optional = {
             max_tokens = 512,
@@ -98,7 +98,34 @@ local ai_list = {
         },
       },
     }
+  },
+  aider = {
+    "GeorgesAlkhouri/nvim-aider",
+    cmd = "Aider",
+    -- Example key mappings for common actions:
+    keys = {
+      { "<leader>a/", "<cmd>Aider toggle<cr>",       desc = "Toggle Aider" },
+      { "<leader>as", "<cmd>Aider send<cr>",         desc = "Send to Aider",                  mode = { "n", "v" } },
+      { "<leader>ac", "<cmd>Aider command<cr>",      desc = "Aider Commands" },
+      { "<leader>ab", "<cmd>Aider buffer<cr>",       desc = "Send Buffer" },
+      { "<leader>a+", "<cmd>Aider add<cr>",          desc = "Add File" },
+      { "<leader>a-", "<cmd>Aider drop<cr>",         desc = "Drop File" },
+      { "<leader>ar", "<cmd>Aider add readonly<cr>", desc = "Add Read-Only" },
+      { "<leader>aR", "<cmd>Aider reset<cr>",        desc = "Reset Session" },
+      -- Example nvim-tree.lua integration if needed
+      { "<leader>a+", "<cmd>AiderTreeAddFile<cr>",   desc = "Add File from Tree to Aider",    ft = "NvimTree" },
+      { "<leader>a-", "<cmd>AiderTreeDropFile<cr>",  desc = "Drop File from Tree from Aider", ft = "NvimTree" },
+    },
+    dependencies = {
+      "folke/snacks.nvim",
+    },
+    opts = {
+      win = {
+        position = "float"
+      }
+    }
+    -- config = true,
   }
 }
 
-return { ai_list.minuet }
+return { ai_list.minuet, ai_list.aider }
